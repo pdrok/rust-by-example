659551693210745fffc1f24e4a0957307b259ce9
Pedro López <cde.luis@gmail.com>
2024-04-03 22:14:06 +0000

    WIP: Chapt custome types

--- src/custom_types/structures.rs
+++ src/custom_types/structures.rs
@@ -24,39 +24,45 @@ struct Rectangle {
     // A rectangle can be specified by where the top left and botton right
     // corners are in space.
     top_left: Point,
-    bottom_right: Point, 
+    bottom_right: Point,
 }
 
-fn main(){
+fn main() {
     // Create struct with field init shorthand
     let name = String::from("Peter");
     let age = 27;
     let peter = Person { name, age };
 
-    // Print debug struct 
+    // Print debug struct
     println!("{:?}", peter);
 
     // Instantiate a `Point`
-    let point: Point = Point { x: 10.3, y: 0.4};
+    let point: Point = Point { x: 10.3, y: 0.4 };
 
     // Access the fields of the point
     println!("point coordinates: ({}, {})", point.x, point.y);
 
     // Make a new point by using struct update syntax to use the fields of our
     // other one
-    let bottom_right = Point { x: 5.2, ..point};
+    let bottom_right = Point { x: 5.2, ..point };
 
     // `bottom_right.y` will be the same as `point.y` because we used that field
     // from `point`
     println!("second point: ({}, {})", bottom_right.x, bottom_right.y);
 
     // Destructure the point using a `let` binding
-    let Point { x: left_edge, y: top_edge} = point;
+    let Point {
+        x: left_edge,
+        y: top_edge,
+    } = point;
 
     let _rectangle = Rectangle {
         // struct instantiation is and expression too
-        top_left: Point {x: left_edge, y: top_edge},
-        bottom_right: bottom_right
+        top_left: Point {
+            x: left_edge,
+            y: top_edge,
+        },
+        bottom_right: bottom_right,
     };
 
     // Instantiate a unit struct
